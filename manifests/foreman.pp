64a5554a8b012ff82d20096fced6d682cc776d9c
Steven Bambling <smbambling@gmail.com>
2014-12-11 09:20:52 -0500

    adding foreman plugins repo

--- manifests/foreman.pp
+++ manifests/foreman.pp
@@ -1,11 +1,17 @@
 # Add the Puppet Labs Products and Dependencies Repositories
 class yumrepo::foreman (
-  $foreman_url         = $yumrepo::params::foreman_url,
-  $foreman_enabled     = $yumrepo::params::foreman_enabled,
-  $foreman_gpgcheck    = $yumrepo::params::foreman_gpgcheck,
-  $foreman_includepkgs = $yumrepo::params::foreman_includepkgs,
-  $foreman_exclude     = $yumrepo::params::foreman_exclude,
-  $foreman_descr        = $yumrepo::params::foreman_descr,
+  $foreman_url                 = $yumrepo::params::foreman_url,
+  $foreman_enabled             = $yumrepo::params::foreman_enabled,
+  $foreman_gpgcheck            = $yumrepo::params::foreman_gpgcheck,
+  $foreman_includepkgs         = $yumrepo::params::foreman_includepkgs,
+  $foreman_exclude             = $yumrepo::params::foreman_exclude,
+  $foreman_descr               = $yumrepo::params::foreman_descr,
+  $foreman_plugins_url         = $yumrepo::params::foreman_plugins_url,
+  $foreman_plugins_enabled     = $yumrepo::params::foreman_plugins_enabled,
+  $foreman_plugins_gpgcheck    = $yumrepo::params::foreman_plugins_gpgcheck,
+  $foreman_plugins_includepkgs = $yumrepo::params::foreman_plugins_includepkgs,
+  $foreman_plugins_exclude     = $yumrepo::params::foreman_plugins_exclude,
+  $foreman_plugins_descr       = $yumrepo::params::foreman_plugins_descr,
 ) {
 
   include yumrepo::params
@@ -34,6 +40,18 @@ class yumrepo::foreman (
       gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-foreman',
       require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-foreman'],
     }
+
+    yumrepo { 'foreman_plugins':
+      descr       => $foreman_plugins_descr,
+      baseurl     => $foreman_plugins_url,
+      enabled     => $foreman_plugins_enabled,
+      gpgcheck    => $foreman_plugins_gpgcheck,
+      includepkgs => $foreman_plugins_includepkgs,
+      exclude     => $foreman_plugins_exclude,
+      gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-foreman',
+      require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-foreman'],
+    }
+
   } else {
     notice ('Your system will not have the Foreman repository applied, requires Operating System Major release of 6 or 7')
   }
