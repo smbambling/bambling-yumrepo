c8209bbceeccb0eaeaaa390d1e0d7fc5f8f8975c
Steven Bambling <smbambling@gmail.com>
2014-12-11 12:17:57 -0500

    fixing url issue

--- manifests/params.pp
+++ manifests/params.pp
@@ -56,7 +56,7 @@ class yumrepo::params {
   $postgresql_9_3_descr       = 'PostgreSQL 9.3 $releasever - $basearch $basearch'
 
   # Elasticsearch 1.4.x Parameters
-  $elasticsearch_1_4_url         = 'http://packages.elasticsearch_1_4.org/elasticsearch_1_4/1.4/centos'
+  $elasticsearch_1_4_url         = 'http://packages.elasticsearch_1_4.org/elasticsearch/1.4/centos'
   $elasticsearch_1_4_enabled     = '1'
   $elasticsearch_1_4_gpgcheck    = '1'
   $elasticsearch_1_4_includepkgs = absent
