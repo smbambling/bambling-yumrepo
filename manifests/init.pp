55e42dc84ea64d9c885084849fd40a24aaab3ebc
Steven Bambling <smbambling@gmail.com>
2014-12-11 12:21:29 -0500

    adding list yum variables command

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,8 @@
 #
 # This class is intentionally empty
 #
+# Command to list yumrepo variables
+# python -c 'import yum, pprint; yb = yum.YumBase(); pprint.pprint(yb.conf.yumvar, width=1)'
 class yumrepo {
 
 }
