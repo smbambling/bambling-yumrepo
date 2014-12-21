b965964159d08d9230c65be4cb3497b8e2ed3eb8
Steven Bambling <smbambling@gmail.com>
2014-12-21 17:25:54 -0500

    moving cleanall to a class

--- manifests/cleanall.pp
+++ manifests/cleanall.pp
@@ -5,7 +5,7 @@
 #    : Clean all cached files from any enabled repository.
 #  [yum makecahce]
 #    : Download and make usable all the metadata for the currently enabled yum repos
-define yumrepo::cleanall () {
+class yumrepo::cleanall () {
   exec { 'cleanall':
     path        => '/bin:/usr/bin:/sbin:/usr/sbin',
     command     => 'yum clean all',
