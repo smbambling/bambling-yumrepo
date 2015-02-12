6e716a1984e1898cbabd1bd5fa5e1e253fdd139e
Steven Bambling <smbambling@gmail.com>
2015-02-12 08:10:43 -0500

    adding manifest to create scl repo

---
+++ manifests/scl.pp
@@ -0,0 +1,23 @@
+# Add the The Software Collections ( SCL ) repository
+class yumrepo::scl (
+  $scl_url         = $yumrepo::params::scl_url,
+  $scl_enabled     = $yumrepo::params::scl_enabled,
+  $scl_gpgcheck    = $yumrepo::params::scl_gpgcheck,
+  $scl_includepkgs = $yumrepo::params::scl_includepkgs,
+  $scl_exclude     = $yumrepo::params::scl_exclude,
+  $scl_descr       = $yumrepo::params::scl_descr,
+) inherits yumrepo::params {
+
+  include yumrepo::cleanall
+
+  yumrepo { 'scl':
+    descr       => $scl_descr,
+    baseurl     => $scl_url,
+    enabled     => $scl_enabled,
+    gpgcheck    => $scl_gpgcheck,
+    includepkgs => $scl_includepkgs,
+    exclude     => $scl_exclude,
+    gpgkey      => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${yumrepo::params::os_maj_release}",
+    notify      => [ Exec['cleanall'], Exec['makecache'] ],
+  }
+} 
