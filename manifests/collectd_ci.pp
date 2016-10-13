3e80dec29c89cd3197941b8201e4535243dd63e5
Steven Bambling <smbambling@gmail.com>
2016-10-13 06:58:34 -0400

    fixing typos and missing rpm key

--- manifests/collectd_ci.pp
+++ manifests/collectd_ci.pp
@@ -1,11 +1,11 @@
 # Add the Extra Packages for Enterprise Linux Repository
-class yumrepo::collect_ci (
-  $collect_ci_url         = $yumrepo::params::collect_ci_url,
-  $collect_ci_enabled     = $yumrepo::params::collect_ci_enabled,
-  $collect_ci_gpgcheck    = $yumrepo::params::collect_ci_gpgcheck,
-  $collect_ci_includepkgs = $yumrepo::params::collect_ci_includepkgs,
-  $collect_ci_exclude     = $yumrepo::params::collect_ci_exclude,
-  $collect_ci_descr       = $yumrepo::params::collect_ci_descr,
+class yumrepo::collectd_ci (
+  $collectd_ci_url         = $yumrepo::params::collect_ci_url,
+  $collectd_ci_enabled     = $yumrepo::params::collect_ci_enabled,
+  $collectd_ci_gpgcheck    = $yumrepo::params::collect_ci_gpgcheck,
+  $collectd_ci_includepkgs = $yumrepo::params::collect_ci_includepkgs,
+  $collectd_ci_exclude     = $yumrepo::params::collect_ci_exclude,
+  $collectd_ci_descr       = $yumrepo::params::collect_ci_descr,
 ) inherits yumrepo::params {
 
   include yumrepo::cleanall
@@ -15,7 +15,7 @@ class yumrepo::collect_ci (
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
-    source => "puppet:///modules/yumrepo/collect_ci/RPM-GPG-KEY-Collectd-CI",
+    source => "puppet:///modules/yumrepo/collectd_ci/RPM-GPG-KEY-Collectd-CI",
   }
 
   yumrepo::rpm_gpg_key { "Collectd-CI":
@@ -23,13 +23,13 @@ class yumrepo::collect_ci (
     before => Yumrepo['collect_ci'],
   }
 
-  yumrepo { 'collect_ci':
-    descr       => $collect_ci_descr,
-    baseurl     => $collect_ci_url,
-    enabled     => $collect_ci_enabled,
-    gpgcheck    => $collect_ci_gpgcheck,
-    includepkgs => $collect_ci_includepkgs,
-    exclude     => $collect_ci_exclude,
+  yumrepo { 'collectd_ci':
+    descr       => $collectd_ci_descr,
+    baseurl     => $collectd_ci_url,
+    enabled     => $collectd_ci_enabled,
+    gpgcheck    => $collectd_ci_gpgcheck,
+    includepkgs => $collectd_ci_includepkgs,
+    exclude     => $collectd_ci_exclude,
     gpgkey      => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Collectd-CI",
     require     => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-Collectd-CI"],
     notify      => [ Exec['cleanall'], ],
